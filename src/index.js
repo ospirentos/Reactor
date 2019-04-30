7dd52e8de2332e5d6f875cc652f0b82454d6d9c3
Ospirentos <ospirentos@gmail.com>
2019-04-30 17:22:18 +0300

    Proper namings and routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import NavBar from './components/navbar'
-import Routeri from './router'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 import "./index.css"
+import NavBar from './components/navbar'
+import Body from './components/body'
 
+class Route extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route path="/" component={NavBar} />
+        <Route path="/characters" component={Body} />
+      </Router>
+    )
+  }
+}
 
-
-ReactDOM.render(<div><NavBar /><Routeri /></div>, document.getElementById('root'));
+ReactDOM.render(<div><PageRouter /></div>, document.getElementById('root'));
