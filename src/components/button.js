95d7486bd802ff7998f24fe652c15918825b0467
Ospirentos <ospirentos@gmail.com>
2019-04-30 17:03:41 +0300

    fix

---
+++ src/components/button.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+import '../index.css'
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: props.text,
+      color: '#ffffff',
+      cursor: 'default',
+      link: props.link
+    }
+  }
+
+  handleMEnter = () => {
+    this.setState({color: '#00ff99', cursor: 'pointer'})
+  }
+
+  handleMLeave = () => {
+    console.log(window.location.href)
+    this.setState({cursor: 'default'})
+  }
+
+  handleMClick = (e) => {
+    this.setState({color: '#00ff99'})
+  }
+
+  render() {
+    return(
+      <div
+        className="button"
+        onClick={this.handleMClick}
+        onMouseOut={this.handleMLeave}
+        style={{color: this.state.color, cursor: this.state.cursor}}
+      >
+        {this.state.text}
+        <div
+          className="buttonLine"
+          style={{background: this.state.color}}
+        ></div>
+      </div>
+    );
+  }
+}
+
+export default Button;
