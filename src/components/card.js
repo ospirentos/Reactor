72f538a92f57fe2daf6a211f1f6d027d79faf0f9
Ospirentos <ospirentos@gmail.com>
2019-05-03 16:08:50 +0300

    card animations have been added.

--- src/components/card.js
+++ src/components/card.js
@@ -2,27 +2,80 @@ import React, {Component} from 'react'
 import '../index.css'
 import arcaneArcher from '../static/arcaneArcher.png'
 import background from '../static/cardBackground.png'
-
+import { useSpring, animated } from 'react-spring'
+/*
 class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: props.data
+      data: props.data,
+      calc: (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1],
+      trans: (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
     }
+  }
+  
+  componentDidMount() {
+      [propsika, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
   }
 
   render() {
     return(
-      <div className="card">
+      <div class="card">
         <div className="cardStructure" 
           style={{backgroundImage: `url(${background})`}}>
         </div>
-        <img className="cardImage" src={arcaneArcher} />
+        <animated.div
+          class="test"
+          onMouseMove={({ clientX: x, clientY: y }) => this.set({ xys: this.state.calc(x, y) })}
+          onMouseLeave={() => this.set({ xys: [0, 0, 1] })}
+          style={{ transform: this.propsika.xys.interpolate(this.state.trans)}}
+        />
+        <div className="cardImage" style={{backgroundImage: `url(${arcaneArcher})`}}></div>
         <div className="cardName">Arcane Archer</div>
         <div className="cardDescription">{this.state.data.text}</div>
       </div>
     );
   }
+}
+*/
+
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+function Card() {
+  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+  return (
+    <div class="card">
+        <animated.div
+          class="cardStructure"
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+          onMouseLeave={() => set({ xys: [0, 0, 1] })}
+          style={{ transform: props.xys.interpolate(trans), backgroundImage: `url(${background})` }}
+        />
+        <animated.div
+          class="cardImage"
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+          onMouseLeave={() => set({ xys: [0, 0, 1] })}
+          style={{ transform: props.xys.interpolate(trans), backgroundImage: `url(${arcaneArcher})` }}
+        />
+        <animated.div
+          class="cardName"
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+          onMouseLeave={() => set({ xys: [0, 0, 1] })}
+          style={{ transform: props.xys.interpolate(trans) }}
+        >
+        Arcane Archer
+        </animated.div>
+        <animated.div
+          class="cardDescription"
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+          onMouseLeave={() => set({ xys: [0, 0, 1] })}
+          style={{ transform: props.xys.interpolate(trans) }}
+        >
+        One of the most bad-ass archers in Tolosis
+        </animated.div>
+      </div>
+  )
 }
 
 export default Card;
